ee659ea158b80029f50d6eb0d01635e416c0c6c0
ivaan7 <ivaanboss7@gmail.com>
2017-01-19 23:02:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.didyoufeelit;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Displays the perceived strength of a single earthquake event based on responses from people who
+ * felt the earthquake.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /** URL for earthquake data from the USGS dataset */
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
+
+    Event earthquake;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BackgroundTask backgroundTask = new BackgroundTask();
+        backgroundTask.execute(USGS_REQUEST_URL);
+
+    }
+
+
+    private class BackgroundTask extends AsyncTask<String,Void,Event>{
+
+
+        @Override
+        protected Event doInBackground(String... params) {
+            if (params.length < 1 || params[0] == null) {
+                return null;
+            }
+            String url = params[0];
+            Event earthquake = Utils.fetchEarthquakeData(url);
+            return earthquake;
+
+        }
+
+        @Override
+        protected void onPostExecute(Event e) {
+            if (e == null) {
+                return;
+            }
+            TextView titleTextView = (TextView) findViewById(R.id.title);
+            titleTextView.setText(e.title);
+            TextView number = (TextView) findViewById(R.id.number_of_people);
+            number.setText(e.numOfPeople);
+            TextView mag = (TextView) findViewById(R.id.perceived_magnitude);
+            mag.setText(e.perceivedStrength);
+        }
+    }
+
+}
